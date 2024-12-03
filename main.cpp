7d417ba52443abfcb12059925fe50303967d0157
Adham Nasreldin <adhamnasreldin16@gmail.com>
2024-12-03 16:58:28 +0200

    initializing repo

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream> 
+
+
+using namespace std ; 
+
+int main (){
+
+    cout << "We're the Data structures team" ; 
+
+    return 0 ; 
+}
+/* Team members : 
+- Youssef Walid Abdelmoez 2101063
+- Yahia Abdalah Saber 2101086
+- Adham Nasreldin Mahmoud 2101137
+- Khaled Mohamed Mohamed Ali 2100689
+- Mahmoud Mostafa Mahmoud 2100403
+- ⁠ahmed mohamed ahmed 2100299
+- ⁠mariam ahmed mohamed 2100433
+*/
