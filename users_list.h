9c78308214990cb541f36fa5ed7ef9e102998d13
Ahmed khadr <168470917+AhmedKhadr1@users.noreply.github.com>
2024-12-30 01:40:50 +0300

    Update and rename users_tree.h to users_list.h

--- users_list.h
+++ users_list.h
@@ -1,14 +1,14 @@
-#ifndef USERS_TREE_H
-#define USERS_TREE_H
+#ifndef USERS_LIST_H
+#define USERS_LIST_H
 
 #include "user.h"
 
-class users_tree {
+class users_list {
 private:
     vector<user> users;
 
 public:
-    users_tree() {}
+    users_list() {}
     void set_all_users(vector<vector<string>> all_users);
     void set_users(vector<string> input_data);
     vector<string> search_topic(string topic_name);
