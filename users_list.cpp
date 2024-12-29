f542b6399762eb16a1828297faa16758d00932df
Ahmed khadr <168470917+AhmedKhadr1@users.noreply.github.com>
2024-12-30 01:41:50 +0300

    Update and rename users_tree.cpp to users_list.cpp

--- users_list.cpp
+++ users_list.cpp
@@ -1,17 +1,17 @@
-#include "./users_tree.h"
+#include "./users_list.h"
 
-void users_tree::set_all_users(vector<vector<string>> all_users) {
+void users_list::set_all_users(vector<vector<string>> all_users) {
     for (auto user : all_users) {
         set_users(user);
     }
 }
-void users_tree::set_users(vector<string> input_data) {
+void users_list::set_users(vector<string> input_data) {
     user new_user;
     new_user.set_topics(input_data);
     users.push_back(new_user);
 }
 
-vector<string> users_tree::search_topic(string topic_name) {
+vector<string> users_list::search_topic(string topic_name) {
     vector<string> result;
     for (auto u : users) {
         auto res = u.get_posts_of_topic(topic_name);
@@ -20,7 +20,7 @@ vector<string> users_tree::search_topic(string topic_name) {
     return result;
 }
 
-vector<string> users_tree::searchword(string word) {
+vector<string> users_list::searchword(string word) {
     vector<string> result;
     for (auto u : users) {
         auto res = u.searchword(word);
